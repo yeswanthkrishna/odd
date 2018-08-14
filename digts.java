9317c812eda274cfee406fb66526f543ea116094
yeswanthkrishna <41936531+yeswanthkrishna@users.noreply.github.com>
2018-08-14 11:10:00 +0530

    Create digts.java

---
+++ digts.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int count = 0, num;
+		Scanner in=new Scanner(System.in);
+		num=in.nextInt();
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+	}
+}
