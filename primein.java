59723380b0d55eee495fd350c7dcb39bc8d94e49
yeswanthkrishna <41936531+yeswanthkrishna@users.noreply.github.com>
2018-08-20 11:38:53 +0530

    Create primein.java

---
+++ primein.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,j,a,b,count=0;
+		Scanner scan=new Scanner(System.in);
+		a=scan.nextInt();
+		b=scan.nextInt();
+		for(i=a+1;i<b;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				if(i % j == 0)
+				{
+					count+=1;
+				}
+					
+			}
+			if(count==2)
+					System.out.println(i);
+					count=0;
+		}
+	}
+}
