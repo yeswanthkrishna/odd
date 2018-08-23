243078c01a722fb33987a086335f93cab3c0293f
yeswanthkrishna <41936531+yeswanthkrishna@users.noreply.github.com>
2018-08-23 07:08:45 +0530

    Create 5multipls.java

---
+++ 5multipls.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,n,x=5,y;
+		Scanner s=new Scanner (System.in);
+		n=s.nextInt();
+		for(i =1;i<= x;i++)
+		{
+			 y = n*i;
+		System.out.println(y);
+		} 
+	}
+}
